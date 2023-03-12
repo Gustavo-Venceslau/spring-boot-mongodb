6f810999883e7d5e21ac7d11a86d8f43e176b0e8
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-03-11 23:47:15 -0300

    Connecting to mongoDB with repository and service

--- src/main/java/com/gustavodealmeida/workshopmongo/domain/User.java
+++ src/main/java/com/gustavodealmeida/workshopmongo/domain/User.java
@@ -1,11 +1,17 @@
 package com.gustavodealmeida.workshopmongo.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Document
 public class User implements Serializable {
 
     private static final long seralVersionUID = 1L;
+
+    @Id
     private String id;
     private String name;
     private String email;

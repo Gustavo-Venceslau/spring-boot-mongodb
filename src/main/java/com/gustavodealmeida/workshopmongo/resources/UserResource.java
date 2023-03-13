51b3c4ba410da5c1d0005fb2cd453c97e7685f54
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-03-13 19:45:40 -0300

    update user with POST

--- src/main/java/com/gustavodealmeida/workshopmongo/resources/UserResource.java
+++ src/main/java/com/gustavodealmeida/workshopmongo/resources/UserResource.java
@@ -40,8 +40,16 @@ public class UserResource {
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<UserDTO> delete(@PathVariable String id){
+    public ResponseEntity<Void> delete(@PathVariable String id){
         service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}" ,method = RequestMethod.PUT)
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id){
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
         return ResponseEntity.noContent().build();
     }
 }

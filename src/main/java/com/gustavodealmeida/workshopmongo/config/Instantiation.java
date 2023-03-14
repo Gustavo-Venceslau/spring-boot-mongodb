c805d176ef780a9e6cd8e4d241455b6d9f5aed85
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-03-14 18:32:56 -0300

    Referencing user posts

--- src/main/java/com/gustavodealmeida/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustavodealmeida/workshopmongo/config/Instantiation.java
@@ -40,7 +40,9 @@ public class Instantiation implements CommandLineRunner {
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para são paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
 
-
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }

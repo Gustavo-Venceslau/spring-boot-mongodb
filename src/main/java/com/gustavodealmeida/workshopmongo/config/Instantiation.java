a13d52470b3b4babd442f8e2a43dc2167efd1f67
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-03-14 18:23:01 -0300

    projection user data with DTO

--- src/main/java/com/gustavodealmeida/workshopmongo/config/Instantiation.java
+++ src/main/java/com/gustavodealmeida/workshopmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.gustavodealmeida.workshopmongo.config;
 
 import com.gustavodealmeida.workshopmongo.domain.Post;
 import com.gustavodealmeida.workshopmongo.domain.User;
+import com.gustavodealmeida.workshopmongo.dto.AuthorDTO;
 import com.gustavodealmeida.workshopmongo.repositories.PostRepository;
 import com.gustavodealmeida.workshopmongo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para são paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", maria);
-
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem!", "Vou viajar para são paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia!", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/gustavodealmeida/workshopmongo/dto/AuthorDTO.java
@@ -0,0 +1,38 @@
+package com.gustavodealmeida.workshopmongo.dto;
+
+import com.gustavodealmeida.workshopmongo.domain.User;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+public class AuthorDTO implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String name;
+
+    public AuthorDTO(){}
+
+    public AuthorDTO(User obj) {
+        this.id = obj.getId();
+        this.name = obj.getName();
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}

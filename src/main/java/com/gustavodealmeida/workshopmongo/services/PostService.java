cb217eb935efdc9cc60e3a6f02c03b50bd81c67e
Gustavo-de-Almeida <gustavodealmeida01@gmail.com>
2023-03-14 22:44:04 -0300

    Query with multiple criteria

--- src/main/java/com/gustavodealmeida/workshopmongo/services/PostService.java
+++ src/main/java/com/gustavodealmeida/workshopmongo/services/PostService.java
@@ -6,6 +6,7 @@ import com.gustavodealmeida.workshopmongo.repositories.PostRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +27,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repository.fullSearch(text, minDate, maxDate);
     }
 }
